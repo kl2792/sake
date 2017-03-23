bb34e6dbc1cd1ed72a71249eda1d48d7d29bdf6d
EmmaLouise2018 <ele2116@columbia.edu>
2017-03-23 19:52:29 -0400

    Update sake-new.ml

--- ocaml/sake-new.ml
+++ ocaml/sake-new.ml
@@ -4,5 +4,5 @@ let header_name = Sys.argv.(1) ^ ".h" and llvm_name = Sys.argv.(1) ^ ".cpp" in
    let lexbuf = Lexing.from_channel in_channel in 
    let program = Parser.program Scanner.token lexbuf in
    let sast = Semantic.check_program program in 
-   let header = Header_generator.string_of_sast sast and llvm = LLVM_generator.string_of_sast sast 
+   let header = Header_generator.string_of_sast sast and llvm = Llvm_generator.string_of_sast sast 
    in Printf.fprintf (open_out header_name) "%s" header; Printf.fprintf (open_out llvm_name) "%s" llvm
