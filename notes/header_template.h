34dd7cb0571261c27fde5faba5b555a24141e3c7
Emma Etherington <ele2116@columbia.edu>
2017-03-23 14:24:00 -0400

    sample template that we will form from parsing the ast

---
+++ notes/header_template.h
@@ -0,0 +1,25 @@
+#ifndef __NAME_H__ // Where name is the name of the sk file
+#define __NAME_H__
+
+enum name_fsmName_state_t { /* STATES in this fsm */ }
+enum name_fsmName2_state_t { /* etc for all fsms in the file */ } 
+...
+
+enum name_enum_[enum_name]_t { /* enum types user defined in sk file */ } 
+...
+
+struct name_input { /* input definition */ }
+struct name_output { /* output definition */ }
+
+struct name_state { 
+    enum name_fsmName_state_t _current_fsmName; /* Will hold current state of the fsm */
+    ... /* do this for all fsm in file */
+
+    /* ALL LOCAL VARAIBLES THAT EXISTS IN ALL FSM 
+     * variable names: fsmName_variableName 
+     */ 
+}
+
+int name_tick(struct name_state *, struct name_input *, struct name_output *);
+
+#endif 
