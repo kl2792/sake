dbd743a9567f7c440c893466349a4919f71b565c
Kai-Zhan Lee <kl2792@users.noreply.github.com>
2017-03-23 14:17:19 -0400

    Remove extraneous character

--- ocaml/sake.ml
+++ ocaml/sake.ml
@@ -1,4 +1,4 @@
-(* Generating the c header file *)_
+(* Generating the C header file and C++ LLVM code *)
 let header_name = Sys.argv.(1) ^ ".h" and llvm_name = Sys.argv.(1) ^ ".cpp" in 
    let in_channel = open_in Sys.argv.(1) in
    let lexbuf = Lexing.from_channel in_channel in 
@@ -6,3 +6,4 @@ let header_name = Sys.argv.(1) ^ ".h" and llvm_name = Sys.argv.(1) ^ ".cpp" in
    let sast = Semantic.check_program program in 
    let header = Header_generator.string_of_sast sast and llvm = LLVM_generator.string_of_sast sast 
    in Printf.fprintf (open_out header_name) "%s" header; Printf.fprintf (open_out llvm_name) "%s" llvm
+ 
