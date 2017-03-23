3e16bdb903d38beabc8b6ccb75c8c4d6cc4ca3f1
Kai-Zhan Lee <kl2792@users.noreply.github.com>
2017-03-23 14:17:27 -0400

    Update sake.ml

--- ocaml/sake.ml
+++ ocaml/sake.ml
@@ -6,4 +6,3 @@ let header_name = Sys.argv.(1) ^ ".h" and llvm_name = Sys.argv.(1) ^ ".cpp" in
    let sast = Semantic.check_program program in 
    let header = Header_generator.string_of_sast sast and llvm = LLVM_generator.string_of_sast sast 
    in Printf.fprintf (open_out header_name) "%s" header; Printf.fprintf (open_out llvm_name) "%s" llvm
- 
