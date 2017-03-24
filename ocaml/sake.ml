f3aeb191e32752c0003d36c36e38f8ae814e9e7a
Kai-Zhan Lee <kl2792@columbia.edu>
2017-03-23 21:02:31 -0400

    Modify sake to include file name in generator calls

--- ocaml/sake.ml
+++ ocaml/sake.ml
@@ -1,8 +1,11 @@
 (* Generating the C header file and C++ LLVM code *)
-let header_name = Sys.argv.(1) ^ ".h" and llvm_name = Sys.argv.(1) ^ ".cpp" in 
-   let in_channel = open_in Sys.argv.(1) in
-   let lexbuf = Lexing.from_channel in_channel in 
-   let program = Parser.program Scanner.token lexbuf in
-   let sast = Semantic.check_program program in 
-   let header = Header_generator.string_of_sast sast and llvm = Llvm_generator.string_of_sast sast 
-   in Printf.fprintf (open_out header_name) "%s" header; Printf.fprintf (open_out llvm_name) "%s" llvm
+let name = Sys.argv.(1) in 
+  let header_name = name ^ ".h" and llvm_name = name ^ ".cpp" in 
+  let in_channel = open_in Sys.argv.(1) in
+  let lexbuf = Lexing.from_channel in_channel in 
+  let program = Parser.program Scanner.token lexbuf in
+  let sast = Semantic.check_program program in 
+  let header = Header_generator.string_of_sast sast name
+      and llvm = Llvm_generator.string_of_sast sast name in
+    Printf.fprintf (open_out header_name) "%s" header;
+    Printf.fprintf (open_out llvm_name) "%s" llvm
