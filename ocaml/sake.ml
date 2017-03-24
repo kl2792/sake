8132a9e50cea13c8240c56f10ee0382c13b3b410
Kai-Zhan Lee <kl2792@columbia.edu>
2017-03-24 00:06:23 -0400

    switch order of arguments to generators

--- ocaml/sake.ml
+++ ocaml/sake.ml
@@ -5,7 +5,7 @@ let name = Sys.argv.(1) in
   let lexbuf = Lexing.from_channel in_channel in 
   let program = Parser.program Scanner.token lexbuf in
   let sast = Semantic.check_program program in 
-  let header = Header_generator.string_of_sast sast name
-      and llvm = Llvm_generator.string_of_sast sast name in
+  let header = Header_generator.string_of_sast name sast
+      and llvm = Llvm_generator.string_of_sast name sast in
     Printf.fprintf (open_out header_name) "%s" header;
     Printf.fprintf (open_out llvm_name) "%s" llvm
