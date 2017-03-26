5010124dc6c3a68b0e74dd1ca67992fe1b112a17
Emma Etherington <ele2116@columbia.edu>
2017-03-26 13:38:46 -0400

    template update

--- notes/header_template.h
+++ notes/header_template.h
@@ -8,8 +8,13 @@ enum name_fsmName2_state_t { /* etc for all fsms in the file */ }
 enum name_[enum_name]_enum_t { /* enum types user defined in sk file */ } 
 ...
 
-struct name_input { /* input definition */ }
-struct name_output { /* output definition */ }
+struct name_input { /* input definition */ 
+        inputType fsmName_variableName;
+        ...
+}
+struct name_output { /* output definition */ 
+        outputType fsmName_variableName;
+}
 
 struct name_state { 
     enum name_fsmName_state_t fsmName; /* Will hold current state of the fsm */
@@ -18,6 +23,7 @@ struct name_state {
     /* ALL LOCAL VARAIBLES THAT EXISTS IN ALL FSM 
      * variable names: fsmName_variableName 
      */ 
+    localVarType fsmName_variableName
 }
 
 int name_tick(struct name_state *, struct name_input *, struct name_output *);
