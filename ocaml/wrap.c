67032e10e7a96f4d2c2ecde799e28bc436ff8541
Arunavha Chanda <ac3806@columbia.edu>
2017-03-27 17:57:11 -0400

    Wrap file

---
+++ ocaml/wrap.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <wrap.h>
+
+int main(){
+	struct name_state s;
+	struct name_input i;
+	struct name_output o;
+	name_tick()
+	struct name_state *, struct name_input *, struct name_output *
+}
