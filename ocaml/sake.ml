24716670c700fbaecb174c2f0057e2c3dc09f27e
Emma Etherington <ele2116@columbia.edu>
2017-03-27 21:12:47 -0400

    cahnge llvm file from cpp to ll

--- ocaml/sake.ml
+++ ocaml/sake.ml
@@ -1,6 +1,6 @@
 (* Generating the C header file and C++ LLVM code *)
 let name = String.lowercase_ascii Sys.argv.(1) in 
-  let header_name = name ^ ".h" and llvm_name = name ^ ".cpp" in 
+  let header_name = name ^ ".h" and llvm_name = name ^ ".ll" in 
   let in_channel = open_in Sys.argv.(1) in
   let lexbuf = Lexing.from_channel in_channel in 
   let ast = Parser.program Scanner.token lexbuf in
