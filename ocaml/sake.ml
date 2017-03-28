92a5399abcf8c87196b254365bf31fd0047f1f79
Shalva-A-Kohen <sak2232@columbia.edu>
2017-03-28 04:01:13 -0400

    added in_channel

--- ocaml/sake.ml
+++ ocaml/sake.ml
@@ -1,6 +1,7 @@
 (* Generating the C header file and C++ LLVM code *)
 let name = Sys.argv.(1) in 
-  let lexbuf = Lexing.from_channel stdin in 
+  let in_channel = open_in Sys.argv.(1) in
+  let lexbuf = Lexing.from_channel in_channel in 
   let ast = Parser.program Scanner.token lexbuf in
   let llvm =  Llvm_generator.translate name ast (* sast *) in
         Llvm_analysis.assert_valid_module m;
