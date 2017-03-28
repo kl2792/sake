dce0805b7d2f654654f5ce4825276c1e05682a2a
EmmaLouise2018 <ele2116@columbia.edu>
2017-03-27 23:27:06 -0400

    those dang s's

--- ocaml/llvm_generator.ml
+++ ocaml/llvm_generator.ml
@@ -24,8 +24,8 @@ let translate program = (* translate an A.program to LLVM *)
     let iter map (dtype, name) =
       StringMap.add name (L.define_global name (init dtype) sake) map in
     List.fold_left iter StringMap.empty lvalues in
-  let inputs = map init program.A.inputs in (* global inputs for concurrent FSM collection *)
-  let outputs = map init program.A.outputs in (* global outputs for concurrent FSM collection *)
+  let inputs = map init program.A.input in (* global inputs for concurrent FSM collection *)
+  let outputs = map init program.A.output in (* global outputs for concurrent FSM collection *)
   let locals = map init program.A.locals in (* fsm write-local state variables *)
   let types = map init program.A.types in (* user-defined types *)
   let states =
