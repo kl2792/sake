3c892c622c110e8c5373508bc2bcead875f92767
EmmaLouise2018 <ele2116@columbia.edu>
2017-03-28 01:21:03 -0400

    draft

--- ocaml/sake.ml
+++ ocaml/sake.ml
@@ -1,11 +1,9 @@
 (* Generating the C header file and C++ LLVM code *)
-let name = String.lowercase_ascii Sys.argv.(1) in 
-  let header_name = name ^ ".h" and llvm_name = name ^ ".ll" in 
-  let in_channel = open_in Sys.argv.(1) in
+let name = Sys.argv.(1) in 
   let lexbuf = Lexing.from_channel in_channel in 
   let ast = Parser.program Scanner.token lexbuf in
-  (* let sast = Semantic.check_program program in *)
-  let header = Header_generator.translate name ast (* sast *)
-    and llvm = Llvm_generator.translate name ast (* sast *) in
-      Printf.fprintf (open_out header_name) "%s" header;
-      Printf.fprintf (open_out llvm_name) "%s" llvm
+  let llvm =  Llvm_generator.translate name ast (* sast *) in
+        Llvm_analysis.assert_valid_module m;
+        print_string (LLvm.string_of_llmodule m);
+
+        (*Printf.fprintf (open_out llvm_name) "%s" llvm*)
