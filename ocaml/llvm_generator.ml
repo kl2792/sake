0ea764a9bd862bb72fb50e3cdd16e295a95b4971
EmmaLouise2018 <ele2116@columbia.edu>
2017-03-27 23:35:54 -0400

    warning and fix

--- ocaml/llvm_generator.ml
+++ ocaml/llvm_generator.ml
@@ -33,8 +33,8 @@ let translate program = (* translate an A.program to LLVM *)
       let init_fsm fsm = (**)
         let rec iter_state map count = function
           | [] -> map
-          | state::tl -> StringMap.add state.A.state_name (L.const_int i32_t count) map;
-              f map (count + 1) tl in
+          | state::tl -> ignore(StringMap.add state.A.state_name (L.const_int i32_t count) map);
+              iter_state map (count + 1) tl in
         List.fold_left iter_state StringMap.empty 0 fsm.A.fsm_body in
       StringMap.add fsm.A.fsm_name (L.define_global fsm.A.fsm_name (init_fsm fsm) sake) map in
     List.fold_left iter StringMap.empty program.A.fsms in
