3b306c3e577396d25f6e15fddab56f7ea54b4d64
Emma Etherington <ele2116@columbia.edu>
2017-04-09 19:38:02 -0400

    c files'

---
+++ testing/test-switch.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "../ocaml/test-switch.h"
+
+int main() {
+    struct test-switch_input i;
+    struct test-switch_output p;
+    struct test-switch_state q;
+    
+    /* CODE FOR CALLING TICK() */
+
+    return 0;
+}
