56ce3b1684908100b67acb9806f64a981ec995cc
Emma Etherington <ele2116@columbia.edu>
2017-04-09 19:25:47 -0400

    this is what the test-wrapper.h should look like for wrapper test case

--- ocaml/test-wrapper.h
+++ ocaml/test-wrapper.h
@@ -1,18 +1,18 @@
 #ifndef __WRAPPER_H__ // Where name is the name of the sk file
 #define __WRAPPER_H__
 
-struct wrapper_input {
+struct test-wrapper_input {
 };
 
-struct wrapper_output {
+struct test-wrapper_output {
 };
 
-struct wrapper_state {
+struct test-wrapper_state {
 };
 
-struct wrapper_tick {
+struct test-wrapper_tick {
 };
 
-void wrapper_tick(struct name_state *, struct name_input *, struct name_output *);
+void test-wrapper_tick(struct name_state *, struct name_input *, struct name_output *);
 
 #endif 
