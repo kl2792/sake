e1944ebe27a6ac814f1181a0c3daa719d1594c0c
Emma Etherington <ele2116@columbia.edu>
2017-04-10 01:34:25 -0400

    file name changes

---
+++ testing/fail-empty.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "fail-empty.h"
+
+int main() {
+	 fail-empty_tick(NULL, NULL, NULL);
+}
---
+++ testing/fail-hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "fail-hello.h"
+
+int main() {
+	 fail-hello_tick(NULL, NULL, NULL);
+}
