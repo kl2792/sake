1cabac98711bd707322efd7869db2a5bfc1456b6
Emma Etherington <ele2116@columbia.edu>
2017-04-10 01:35:10 -0400

    dont need c

--- testing/fail-empty.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "fail-empty.h"
-
-int main() {
-	 fail-empty_tick(NULL, NULL, NULL);
-}
--- testing/fail-hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "fail-hello.h"
-
-int main() {
-	 fail-hello_tick(NULL, NULL, NULL);
-}
