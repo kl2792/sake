30d8fd5f653690015fbdd09f93edcbf0d54d4c56
Emma Etherington <ele2116@columbia.edu>
2017-04-13 18:27:21 -0400

    test c

---
+++ testing/test_ifelse.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test_ifelse.h"
+
+int main() {
+	 test_ifelse_tick(NULL, NULL, NULL);
+}
---
+++ testing/test_var.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test_var.h"
+
+int main() {
+	 test_var_tick(NULL, NULL, NULL);
+}
