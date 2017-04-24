44299025ceb640cd8195bf9e716c1da9e2fc9891
Emma Etherington <ele2116@columbia.edu>
2017-04-23 20:50:09 -0400

    updated tests

---
+++ testing/test_nestedFor.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test_nestedFor.h"
+
+int main() {
+	 test_nestedFor_tick(NULL, NULL, NULL);
+}
---
+++ testing/test_nestedIf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test_nestedIf.h"
+
+int main() {
+	 test_nestedIf_tick(NULL, NULL, NULL);
+}
