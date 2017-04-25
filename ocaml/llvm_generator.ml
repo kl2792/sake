eb6b779a3182217071559f00cc7788b5ef5f456b
Kai-Zhan Lee <kl2792@columbia.edu>
2017-04-25 19:43:45 -0400

    Major LLVM lookup fixes

--- ocaml/llvm_generator.ml
+++ ocaml/llvm_generator.ml
@@ -73,40 +73,33 @@ let init t v = L.const_int (lltype t) v in
     L.declare_function "memcpy" ftype sake in
 
   (* Variables *)
-  let global_vars =
-    let map vars =
-      let merge index map (dtype, name) =
-        let global = L.define_global name (init dtype 0) sake in
-        StringMap.add name (index, global) map in
-      List.fold_left (merge 0) StringMap.empty vars in
-    let inputs = map program.A.input in (* FSM inputs *)
-    let outputs = map program.A.output in (* FSM outputs *)
-    let public = List.map (fun (t, n, _) -> t, n) program.A.public in
-    let public = map public in (* FSM public write-local state variables *)
-    let states = List.map (fun fsm -> (A.Enum ""), fsm.A.fsm_name) program.A.fsms in
-    let states = map states in (* FSM state indicators *)
-    let names = ["input"; "output"; "public"; "state"]
-    and submaps = [inputs; outputs; public; states] in
-    let merge map name submap = StringMap.add name submap map in
-    List.fold_left2 merge StringMap.empty names submaps in
+  let map vars =
+    let merge map (dtype, name, value) = (* TODO: init with e *)
+      let global = L.define_global name (init dtype 0) sake in
+      StringMap.add name global map in
+    List.fold_left merge StringMap.empty vars in
+  let zmap vars = map (List.map (fun (t, n) -> t, n, 0) vars) in
+  let public = map program.A.public in
+  let state = zmap (List.map (fun f -> A.Int, f.A.fsm_name) program.A.fsms) in
+  let input = zmap program.A.input and output = zmap program.A.output in
+  let locals = ref StringMap.empty in
 
   (* Lookup functions *)
-  let locals = ref StringMap.empty in
-  let value name =
+  let value name = (* TODO: delete *)
     let rec find_name i = function
       | [] -> raise (Error "... y tho")
       | enum :: tail -> if enum = name then i else find_name (i + 1) tail in
     find_name 0 program.A.types in
-  let lookup name maps = (* search for given name in specified maps *)
+  let fsm_name = ref "" in
+  let lookup io name = (* search for given name in specified maps *)
     try StringMap.find name !locals
     with Not_found ->
-      let rec search = function
-        | [] -> raise (Error "what the hell?")
-        | map :: maps ->
-            let map = StringMap.find map global_vars in
-            try snd (StringMap.find name map)
-            with Not_found -> search maps in
-      search maps in
+      try StringMap.find (!fsm_name ^ "_" ^ name) public
+      with Not_found ->
+        try StringMap.find name public
+        with Not_found ->
+          try StringMap.find name io
+          with Not_found -> raise (Error "what the hell? sast problem?") in
 
   (* Expression builder *)
   let rec expr builder = function
@@ -115,7 +108,7 @@ let init t v = L.const_int (lltype t) v in
     | A.CharLit c -> L.const_int i8_t (int_of_char c)
     | A.StringLit s -> L.const_stringz context s
     | A.Empty -> L.const_int i32_t 0
-    | A.Variable s -> raise (Error "NIMP: Variable")(*L.build_load (lookup s) s builder*)
+    | A.Variable s -> L.build_load (lookup input s) s builder
     | A.Printf (fmt, args) ->
         let fmt' = L.build_global_stringptr fmt "fmt" builder in
         let args = [fmt'; expr builder (List.hd args)] in
@@ -130,8 +123,9 @@ let init t v = L.const_int (lltype t) v in
         let build = llop op in
         let e1 = expr builder e1 and e2 = expr builder e2 in
         build e1 e2 "tmp" builder
-    | A.Assign (s, e) -> raise (Error "NIMP: Assign")
-(* let e = expr builder e in L.build_store e (lookup s) builder; e *) in
+    | A.Assign (s, e) ->
+        let e = expr builder e in
+        L.build_store e (lookup output s) builder; e in
 
   let add_terminal builder f =
     match L.block_terminator (L.insertion_block builder) with
@@ -169,21 +163,21 @@ let init t v = L.const_int (lltype t) v in
         let case = expr builder predicate in
         let merge_bb = L.append_block context "merge" fn in
         let switch_in = L.build_switch case merge_bb (List.length cases) builder in
-        let rec iter i = function [] -> ()
-          | (c, s) :: tail ->
-              let onval = expr builder c in
-              let case_bb = L.append_block context (Printf.sprintf "case_%d" i) fn in
-              add_terminal (stmt fn (L.builder_at_end context case_bb) s)
-              (L.build_br merge_bb);
-              L.add_case switch_in onval case_bb;
-            iter (i + 1) tail in
-        iter 0 cases; 
+        let i = ref 0 in
+        let  iter (c, s) =
+          let onval = expr builder c in
+          let case_bb = L.append_block context (Printf.sprintf "case_%d" !i) fn in
+          add_terminal (stmt fn (L.builder_at_end context case_bb) s)
+          (L.build_br merge_bb);
+          L.add_case switch_in onval case_bb;
+          i := !i + 1 in
+        List.iter iter cases;
         L.builder_at_end context merge_bb 
-          | A.For (name, iter, body) ->
-              (* TODO: implement local variables for for loop *)
-              raise (Error "stop it")
-          | A.State name -> raise (Error "not this one!")
-          | A.Goto state -> raise (Error "don't be an idiot, goto isn't done yet") in
+    | A.For (name, iter, body) ->
+        (* TODO: implement local variables for for loop *)
+        raise (Error "stop it")
+    | A.State name -> raise (Error "not this one!")
+    | A.Goto state -> raise (Error "don't be an idiot, goto isn't done yet") in
 
   (* FSM functions *)
   let fsms =
@@ -198,6 +192,7 @@ let init t v = L.const_int (lltype t) v in
         let alloca = L.build_alloca (lltype t) n builder in
         StringMap.add n alloca m in
       locals := List.fold_left add_local StringMap.empty fsm.A.fsm_locals;
+      fsm_name := fsm.A.fsm_name;
       add_terminal (stmt fn builder (A.Block fsm.A.fsm_body)) L.build_ret_void;
       fn in
     List.map build_fsm program.A.fsms in
