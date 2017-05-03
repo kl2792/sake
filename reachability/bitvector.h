0aee7b82afb61bc0dca2535fd968046b64df27be
Kai-Zhan Lee <kl2792@columbia.edu>
2017-05-02 21:10:21 -0400

    documentation for bit vector

--- reachability/bitvector.h
+++ reachability/bitvector.h
@@ -1,7 +1,17 @@
+/* Usage examples:
+ * DEFINE_BITVECTOR(vector, 1024); // initialized to all 0s
+ * SETBIT(vector, 1023);           // set the last bit in the vector
+ * CLRBIT(vector, 1023);           // unset the last bit 
+ * TRYBIT(vector, 1023) == 0;      // get the boolean value of the last bit
+ *
+ * Inspired by PantryFS (http://www.cs.columbia.edu/~jae/4118/HW7-pantry.html)
+ * Author: Kai-Zhan Lee
+ */
+
 #ifndef __BITVECTOR_H__
 #define __BITVECTOR_H__
 
-#define DEFINE_BITVECTOR(v, size) unsigned int v[(size) / 32 + 1]  
+#define DEFINE_BITVECTOR(v, size) unsigned int v[(size) / 32 + 1] = { 0 }
 #define SETBIT(v, i) (v[(i) / 32] |=  (1 << ((k) % 32)))
 #define CLRBIT(v, i) (v[(i) / 32] &= ~(1 << ((k) % 32)))
 #define TRYBIT(v, i) (v[(i) / 32] &   (1 << ((k) % 32)))
