f3d33957e35b509dbd96c475b3c2a15310a33190
Arunavha Chanda <ac3806@columbia.edu>
2017-05-03 23:51:33 -0400

    New semantic checking

--- ocaml/semant.ml
+++ ocaml/semant.ml
@@ -224,8 +224,8 @@ let s_list = List.map (fun s -> check_stmt env' fsm s) s_list
 | S.Expr(e) -> ignore (get_expr env e) (**)
 
 | S.Goto(label) ->
-  try List.find (fun (s,_) -> s=label) fsm.S.fsm_states
-with Not_found -> raise (SemanticError "No such state exists")
+  ignore(try List.find (fun (s,_) -> s=label) fsm.S.fsm_states
+with Not_found -> raise (SemanticError "No such state exists"))
 
 
 and check_cases = function (* (expr * stmt) list *)
