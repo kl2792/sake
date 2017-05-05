b02bc3716fd46bde058ba35e88167ab40eca94cc
Emma Etherington <ele2116@columbia.edu>
2017-05-05 15:31:26 -0400

    fsmhello.c

--- testing/test_fsmhello.c
+++ testing/test_fsmhello.c
@@ -10,18 +10,21 @@ int main(){
    
     test_fsmhello_tick(&s, NULL, NULL); 
 
+    i.p = 0; // print nothing
+    test_fsmhello_tick(&s, &i, NULL); 
+
+    i.p = 1; // print hello
+    test_fsmhello_tick(&s, &i, NULL); 
+
     i.p = 0;
+    i.q = "Emma"; //print nothing
     test_fsmhello_tick(&s, &i, NULL); 
 
     i.p = 1;
+    i.q = "Emma"; //print Emma's world 
     test_fsmhello_tick(&s, &i, NULL); 
 
-    i.p = 0;
-    i.q = "Emma";
-    test_fsmhello_tick(&s, &i, NULL); 
-
-    i.p = 1;
-    test_fsmhello_tick(&s, &i, NULL); 
+    // print hello b/c p is still 1
     test_fsmhello_tick(&s, &i, NULL); 
 
     return 0;
