356f3f8cb5c75a51f1455c1f7c3011e830be84b8
Emma Etherington <ele2116@columbia.edu>
2017-05-07 00:26:22 -0400

    ad changes

--- testing/adventure.c
+++ testing/adventure.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-#include "test_adventure.h"
+#include "adventure.h"
 
 int main() {
 
-    struct test_adventure_input input;
-    struct test_adventure_output output;
-    struct test_adventure_state states;
+    struct adventure_input input;
+    struct adventure_output output;
+    struct adventure_state states;
 
     // TODO run tick with null values to do the reset
     output.result = 0;
 
     while (output.result == 0) {
-        printf("Press s to start the adventure");
+        printf("Press s to start the adventure: ");
         scanf("%c", &(input.decision));
-        test_adventure_tick(&states, &input, &output);
+        adventure_tick(&states, &input, &output);
     }
 
     while (output.result == 1) {
         printf("Press the corresponding letter on your keyboard to make your choice: ");
         scanf("%c", &(input.decision));
-        test_adventure_tick(&states, &input, &output);
+        adventure_tick(&states, &input, &output);
     }
 
     if (output.result == 2) {
