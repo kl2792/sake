15d8bc5ff8fb34ac6686a01294868e76963f9029
Emma Etherington <ele2116@columbia.edu>
2017-05-07 15:53:12 -0400

    c

--- testing/adventure.c
+++ testing/adventure.c
@@ -10,15 +10,26 @@ int main() {
     // TODO run tick with null values to do the reset
     output.result = 0;
 
+    char user_input[50];
+
     while (output.result == 0) {
         printf("Press s to start the adventure: ");
-        scanf("%c", &(input.decision));
+        scanf("%s", user_input);
+        input.decision = user_input[0];
+        printf("user input: %c\n", input.decision);
         adventure_tick(&states, &input, &output);
+        printf("after tick\n\n");
     }
+
+    printf("After first state moving onto main loop");
 
     while (output.result == 1) {
         printf("Press the corresponding letter on your keyboard to make your choice: ");
-        scanf("%c", &(input.decision));
+        scanf("%s", (user_input));
+        input.decision = *user_input;
+
+
+        //scanf("%c", &(input.decision));
         adventure_tick(&states, &input, &output);
     }
 
